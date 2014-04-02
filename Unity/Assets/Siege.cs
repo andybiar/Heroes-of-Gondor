e179f95bc6a34ee05a736b7c1c9db6a08982fd09
Andy Biar <andybiar@gmail.com>
2014-04-02 01:51:57 -0400

    title screen and particles

---
+++ Unity/Assets/Siege.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Siege : MonoBehaviour {
+	private bool active;
+	public GameObject target;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		active = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (active) {
+			transform.LookAt(target.transform);
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+	}
+
+	public void activate() {
+		active = true;
+	}
+}
---
+++ Unity/Assets/scripts/movieTex.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movieTex : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (this.guiTexture) {
+			((MovieTexture)guiTexture.texture).loop = true;
+			((MovieTexture)guiTexture.texture).Play();
+		}
+		if (this.renderer.material.mainTexture) {
+			((MovieTexture)renderer.material.mainTexture).loop = true;
+			((MovieTexture)renderer.material.mainTexture).Play();
+		}
+	}
+}
